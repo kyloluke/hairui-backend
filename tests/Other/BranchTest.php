d475931975f1abdaef3c5e087369bbbb4199e084
Your Name <you@example.com>
2023-12-17 20:42:53 +0900

    update

--- tests/Other/BranchTest.php
+++ tests/Other/BranchTest.php
@@ -24,14 +24,12 @@ class BranchTest extends TestCase
 //        dump(base_path().'/only_develop_file.txt');
 
 //        $res = file_exists(realpath(base_path().'/only_develop_file.txt'));
-        $res = file_exists(base_path().'/only_develop_file.txt');
+        $res = file_exists(realpath(base_path().'/only_develop_file.txt'));
         dump($res);
         dump($branch);
 
-
         if($branch == 'develop' && $res) {
-//            throw new \Exception('test error');
-            $this->throwException(new \Exception('test error'));
+            fail('test error');
         } else {
             $this->assertTrue(true);
         }

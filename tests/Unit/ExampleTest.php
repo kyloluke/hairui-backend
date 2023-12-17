0f5833c4ba52a93008b0294c00d145533ebad437
Your Name <you@example.com>
2023-12-17 20:06:29 +0900

    update

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -13,7 +13,6 @@ class ExampleTest extends TestCase
      */
     public function test_example()
     {
-        throw new \Exception('test error');
-//        $this->assertTrue(true);
+        $this->assertTrue(false);
     }
 }

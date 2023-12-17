1d1b1e9e0d40e5a2171300d920d553cada30e0a2
Your Name <you@example.com>
2023-12-17 20:04:02 +0900

    update

---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        throw new \Exception('test error');
+//        $this->assertTrue(true);
+    }
+}
